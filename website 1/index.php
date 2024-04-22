f24160634a47b5d411bb7838e1e45d10c1fdb99d
dhruvdhawan91 <102400001+dhruvdhawan91@users.noreply.github.com>
2024-04-21 19:29:31 -0700

    Add files via upload

---
+++ website 1/index.php
@@ -0,0 +1,23 @@
+<?php
+$maybeallowed = "Dhruv";
+?>
+
+
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <link rel="stylesheet" href="stylesheets/style.css">
+        <h1>This is a header</h1>
+    </head>
+
+    <body>
+        
+        <div class="face">
+            <p1><?php echo $maybeallowed ?></p>
+        <div>
+
+    </body>
+
+</html>
+
